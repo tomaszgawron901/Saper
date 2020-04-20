27f083f3e4a196fd15474481644e9fcebb710829
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-04-20 02:02:42 +0200

    Added controller which enables comunication between Game and GameContainerElement on screen.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 import './main.scss';
-import Board, { OnCellClickArgs} from './components/Board';
-import GameContainer from './components/GameContainer';
-import Game, { GameEvents, OnOpenArgs } from './logic/Game';
-import Position from './logic/Position';
-import EventHandler from './events/EventHandler';
 
-const game = new Game({width: 15, height: 15}, 40);
-const g = new GameContainer();
-g.board.AddOnClickListener((args: OnCellClickArgs) => {
-    game.Open(new Position(args.x, args.y));
-});
+import Controller from './controller';
 
-(game.GetEventHandler(GameEvents.open) as EventHandler<OnOpenArgs>).AddEventListener( (args: OnOpenArgs) => {
-    console.log(args.index, args.value);
-    
-} )
-document.body.appendChild(g.GetComponent());
+const controller = new Controller();
+document.body.appendChild(controller.gameContainerElement.GetComponent());
