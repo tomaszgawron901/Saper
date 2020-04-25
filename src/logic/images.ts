817eed0e2ea9b0e24aa8c28592da6431326639e7
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-04-25 17:44:08 +0200

    created.

---
+++ src/logic/images.ts
@@ -0,0 +1,50 @@
+
+const bomb = new Image(18, 18);
+bomb .src = '../images/bomba.png';
+
+const flag = new Image(18, 18);
+flag.src = '../images/flag.png';
+
+const notbomb = new Image(18, 18);
+notbomb.src = '../images/notbomba.png';
+
+const o1 = new Image(18, 18);
+o1.src = '../images/o1.png';
+
+const o2 = new Image(18, 18);
+o2.src = '../images/o2.png';
+
+const o3 = new Image(18, 18);
+o3.src = '../images/o3.png';
+
+const o4 = new Image(18, 18);
+o4.src = '../images/o4.png';
+
+const o5 = new Image(18, 18);
+o5.src = '../images/o5.png';
+
+const o6 = new Image(18, 18);
+o6.src = '../images/o6.png';
+
+const o7 = new Image(18, 18);
+o7.src = '../images/o7.png';
+
+const o8 = new Image(18, 18);
+o8.src = '../images/o8.png';
+
+
+const Images: any = {
+    'bomb': bomb,
+    'flag': flag,
+    'notBomb': notbomb,
+    '1': o1,
+    '2': o2,
+    '3': o3,
+    '4': o4,
+    '5': o5,
+    '6': o6,
+    '7': o7,
+    '8': o8
+}
+
+export default Images;
