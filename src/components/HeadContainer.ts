8abaf2d94e633339466ee776d5e1d5c2aba8842d
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-04-28 20:22:17 +0200

    Head container created.

---
+++ src/components/HeadContainer.ts
@@ -0,0 +1,33 @@
+import IComponent from "./IComponent";
+import Counter from "./Counter";
+
+export default class HeadContainer implements IComponent{
+    private leftCounter: Counter;
+    private rightCounter: Counter;
+
+    public get LeftCounter(): Counter{
+        return this.leftCounter;
+    }
+
+    public get RigthCounter(): Counter{
+        return this.rightCounter;
+    }
+
+    public constructor(){
+        this.leftCounter = new Counter(3);
+        this.rightCounter = new Counter(3);
+    }
+
+    public Reset(){
+        this.leftCounter.SetValue(0);
+        this.rightCounter.SetValue(0);
+    }
+
+    public GetComponent(){
+        const element = document.createElement('DIV') as HTMLDivElement;
+        element.classList.add("HeadContainer");
+        element.appendChild(this.leftCounter.GetComponent());
+        element.appendChild(this.rightCounter.GetComponent());
+        return element;
+    }
+}
