7d5b6de5643102b76fafc456ab5d6039afe68c64
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-05-04 16:55:23 +0200

    new game button added

--- src/components/HeadContainer.ts
+++ src/components/HeadContainer.ts
@@ -1,9 +1,11 @@
 import IComponent from "./IComponent";
 import Counter from "./Counter";
+import NewGameBTN from "./NewGameBTN";
 
 export default class HeadContainer implements IComponent{
     private leftCounter: Counter;
     private rightCounter: Counter;
+    private newGameBTN: NewGameBTN;
 
     public get LeftCounter(): Counter{
         return this.leftCounter;
@@ -13,9 +15,14 @@ export default class HeadContainer implements IComponent{
         return this.rightCounter;
     }
 
+    public get NewGameBTN(): NewGameBTN{
+        return this.newGameBTN;
+    }
+
     public constructor(){
         this.leftCounter = new Counter(3);
         this.rightCounter = new Counter(3);
+        this.newGameBTN = new NewGameBTN();
     }
 
     public Reset(){
@@ -27,6 +34,7 @@ export default class HeadContainer implements IComponent{
         const element = document.createElement('DIV') as HTMLDivElement;
         element.classList.add("HeadContainer");
         element.appendChild(this.leftCounter.GetComponent());
+        element.appendChild(this.newGameBTN.GetComponent());
         element.appendChild(this.rightCounter.GetComponent());
         return element;
     }
