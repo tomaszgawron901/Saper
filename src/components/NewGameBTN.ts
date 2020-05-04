9403435d4cab01311f15b499d1d8c57537eb59eb
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-05-04 20:02:44 +0200

    SetImage function added.

--- src/components/NewGameBTN.ts
+++ src/components/NewGameBTN.ts
@@ -15,8 +15,20 @@ export default class NewGameBTN extends Clickable<OnNewGameBTNClickArgs> impleme
         this.element = document.createElement('DIV');
         this.element.classList.add('NewGameBTN');
         this.element.classList.add('BorderedContainer');
+        this.element.classList.add('CellEnabled');
 
+        this.SetImage('images/e1.png');
         this.element.addEventListener('click', () => {this.Clicked();});
+    }
+
+    public SetImage(path: string){
+        const img = document.createElement('DIV');
+        img.classList.add('ImgDIV');
+        img.style.backgroundImage = "url("+path+")";
+        img.style.width = "100%";
+        img.style.height = "100%";
+        this.element.innerHTML = "";
+        this.element.appendChild(img);
     }
 
 
