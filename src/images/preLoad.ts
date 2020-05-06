639a2984aa1be8bcb2dda1022bd63e9a08311be1
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-05-06 16:59:02 +0200

    refactored and renamed

---
+++ src/images/preLoad.ts
@@ -0,0 +1,79 @@
+export default function(){
+    const bomb = new Image(18, 18);
+    bomb .src = 'images/bomba.png';
+    
+    const flag = new Image(18, 18);
+    flag.src = 'images/flag.png';
+    
+    const notbomb = new Image(18, 18);
+    notbomb.src = 'images/notbomba.png';
+    
+    const o1 = new Image(18, 18);
+    o1.src = 'images/o1.png';
+    
+    const o2 = new Image(18, 18);
+    o2.src = 'images/o2.png';
+    
+    const o3 = new Image(18, 18);
+    o3.src = 'images/o3.png';
+    
+    const o4 = new Image(18, 18);
+    o4.src = 'images/o4.png';
+    
+    const o5 = new Image(18, 18);
+    o5.src = 'images/o5.png';
+    
+    const o6 = new Image(18, 18);
+    o6.src = 'images/o6.png';
+    
+    const o7 = new Image(18, 18);
+    o7.src = 'images/o7.png';
+    
+    const o8 = new Image(18, 18);
+    o8.src = 'images/o8.png';
+    
+    const c0 = new Image(18, 18);
+    c0.src = 'images/c0.gif';
+    
+    const c1 = new Image(18, 18);
+    c1.src = 'images/c1.gif';
+    
+    const c2 = new Image(18, 18);
+    c2.src = 'images/c2.gif';
+    
+    const c3 = new Image(18, 18);
+    c3.src = 'images/c3.gif';
+    
+    const c4 = new Image(18, 18);
+    c4.src = 'images/c4.gif';
+    
+    const c5 = new Image(18, 18);
+    c5.src = 'images/c5.gif';
+    
+    const c6 = new Image(18, 18);
+    c6.src = 'images/c6.gif';
+    
+    const c7 = new Image(18, 18);
+    c7.src = 'images/c7.gif';
+    
+    const c8 = new Image(18, 18);
+    c8.src = 'images/c8.gif';
+    
+    const c9 = new Image(18, 18);
+    c9.src = 'images/c9.gif';
+    
+    const c_ = new Image(18, 18);
+    c_.src = 'images/c-.gif';
+    
+    const e1 = new Image(32, 32);
+    e1.src = 'images/e1.png';
+    
+    const e2 = new Image(32, 32);
+    e2.src = 'images/e2.png';
+    
+    const e3 = new Image(32, 32);
+    e3.src = 'images/e3.png';
+    
+    const e4 = new Image(32, 32);
+    e4.src = 'images/e4.png';
+}
