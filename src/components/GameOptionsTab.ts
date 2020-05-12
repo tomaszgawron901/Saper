86b4fa47057911d462718930f5393556b66f649e
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-05-12 20:12:45 +0200

    Added: GameOptionsTab, GameOptionsTable classes. Added: GetLAbeledElement function. Added: GameTypes enum.

--- src/components/GameOptionsTab.ts
+++ src/components/GameOptionsTab.ts
@@ -1,67 +1,114 @@
 import IComponent from "./IComponent";
 
+enum GameTypes{
+    beginner = 0,
+    intermediate = 1,
+    expert = 2,
+    custom = 3
+}
+
+function GetLabeledElement(element: HTMLElement, text: string)
+{
+    const label = document.createElement('LABEL') as HTMLLabelElement;
+    const textElement = document.createElement('TEXT');
+    textElement.innerText = text;
+    label.appendChild(element);
+    label.appendChild(textElement);
+    return label;
+}
+
+class GameOptionsTable implements IComponent{
+    private static curentID = 0;
+
+    private table: HTMLTableElement;
+    private radioButtnos: Array<HTMLInputElement>;
+    private id: number;
+
+    private gameType: GameTypes;
+    private customValue: {width: number, height: number, bombs: number};
+
+    public Check(gameType: GameTypes){
+        this.radioButtnos[gameType].checked = true;
+        this.OnSelectionChange(gameType);
+    }
+
+    public constructor(){
+        this.id = GameOptionsTable.curentID++;
+        this.InitializeRadioButtons();
+
+        this.table = document.createElement('TABLE') as HTMLTableElement;
+        this.table.classList.add('OptionsTab');
+        
+        this.AppendRow(0, ["", "Width", "Height", "Bombs"]);
+
+        this.AppendRow(1, [GetLabeledElement(this.radioButtnos[0], "Beginner"), "8", "8", "10"]);
+
+        this.AppendRow(2, [GetLabeledElement(this.radioButtnos[1], "Intermediate"), "16", "16", "40"]);
+
+
+        this.AppendRow(3, [GetLabeledElement(this.radioButtnos[2], "Expert"), "32", "16", "99"]);
+
+        const in1 = document.createElement('INPUT') as HTMLInputElement;
+        in1.style.width = '40px';
+        const in2 = document.createElement('INPUT') as HTMLInputElement;
+        in2.style.width = '40px';
+        const in3 = document.createElement('INPUT') as HTMLInputElement;
+        in3.style.width = '40px';
+        this.AppendRow(4, [GetLabeledElement(this.radioButtnos[3], "Custom"), in1, in2, in3]);
+        this.Check(2);
+    }
+
+    private InitializeRadioButtons(){
+        const numberOfButtons = 4;
+        const name = "GameTypes" + this.id.toString();
+        this.radioButtnos = new Array<HTMLInputElement>(numberOfButtons);
+        for(let i = 0; i < numberOfButtons; i++)
+        {
+            this.radioButtnos[i] = document.createElement("INPUT") as HTMLInputElement;
+            this.radioButtnos[i].setAttribute("type", "radio");
+            this.radioButtnos[i].name = name;
+            this.radioButtnos[i].onchange = () => {
+                this.OnSelectionChange(i);
+            }
+        }
+    }
+
+    private OnSelectionChange(gameType: GameTypes){
+        this.gameType = gameType;
+        console.log(gameType);
+        
+    }
+
+    private AppendRow(index: number = 0, elements: Array<HTMLElement | string>){
+        const row = this.table.insertRow(index);
+        for(let i = elements.length-1; i >= 0; i--)
+        {
+            const cell = row.insertCell(0);
+            if(elements[i] instanceof HTMLElement)
+            {
+                cell.appendChild(elements[i] as HTMLElement);
+            }
+            else if (typeof elements[i] === 'string')
+            {
+                cell.innerHTML = elements[i] as string;
+            }
+        }
+    }
+
+    public GetComponent(){
+        return this.table;
+    }
+}
+
+
 export default class GameOptionsTab implements IComponent{
     private element: HTMLDivElement;
     
     public constructor(){
         this.element = document.createElement('DIV') as HTMLDivElement;
-        this.element.appendChild(this.GetTable());
-    }
-
-    private GetTable(){
-        const table = document.createElement('TABLE') as HTMLTableElement;
-        table.classList.add('OptionsTab');
-
-        const head = table.insertRow(0);
-        head.classList.add('OptionsTabHead');
-        head.insertCell(0)
-        const headCell1 = head.insertCell(1);
-        headCell1.innerText = 'Width';
-        const headCell2 = head.insertCell(2);
-        headCell2.innerText = 'Height';
-        const headCell3 = head.insertCell(3);
-        headCell3.innerText = 'Bombs';
-
-        const row1 = table.insertRow(1);
-        const row1Cell0 = row1.insertCell(0);
-        var row1Radio = document.createElement("INPUT") as HTMLInputElement;
-        row1Radio.setAttribute("type", "radio");
-        row1Radio.name = 'gameType';
-        row1Cell0.appendChild(row1Radio);
-        const row1Cell1 = row1.insertCell(1);
-        row1Cell1.innerText = '8';
-        const row1Cell2 = row1.insertCell(2);
-        row1Cell2.innerText = '8';
-        const row1Cell3 = row1.insertCell(3);
-        row1Cell3.innerText = '10';
-
-        const row2 = table.insertRow(2);
-        const row2Cell0 = row2.insertCell(0);
-        var row2Radio = document.createElement("INPUT") as HTMLInputElement;
-        row2Radio.setAttribute("type", "radio");
-        row2Radio.name = 'gameType';
-        row2Cell0.appendChild(row2Radio);
-        const row2Cell1 = row2.insertCell(1);
-        row2Cell1.innerText = '16';
-        const row2Cell2 = row2.insertCell(2);
-        row2Cell2.innerText = '16';
-        const row2Cell3 = row2.insertCell(3);
-        row2Cell3.innerText = '40';
-
-        const row3 = table.insertRow(3);
-        const row3Cell0 = row3.insertCell(0);
-        var row3Radio = document.createElement("INPUT") as HTMLInputElement;
-        row3Radio.setAttribute("type", "radio");
-        row3Radio.name = 'gameType';
-        row3Cell0.appendChild(row3Radio);
-        const row3Cell1 = row3.insertCell(1);
-        row3Cell1.innerText = '32';
-        const row3Cell2 = row3.insertCell(2);
-        row3Cell2.innerText = '16';
-        const row3Cell3 = row3.insertCell(3);
-        row3Cell3.innerText = '99';
-
-        return table;
+        const got = new GameOptionsTable();
+        
+        this.element.appendChild(got.GetComponent());
     }
 
 
