8a5d7abaffac6516b911c842d3f82909ed8a2d75
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-05-13 17:11:50 +0200

    gameOptionsTab element appended to body

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import Controller from './controller';
 import GameOptionsTab from './components/GameOptionsTab';
 
 PreLoad();
-const gameOptionsTable = new GameOptionsTab();
 const controller = new Controller();
 document.body.appendChild(controller.gameContainerElement.GetComponent());
-document.body.appendChild(gameOptionsTable.GetComponent());
+document.body.appendChild(controller.gameOptionsTab.GetComponent());
