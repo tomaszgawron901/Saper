09fc38ce092f89cfe50073344e64889396049726
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-05-16 22:25:02 +0200

    GetLabeledElement fixed

--- src/components/MenuComponents/GameOptionsMenuTab.ts
+++ src/components/MenuComponents/GameOptionsMenuTab.ts
@@ -11,6 +11,8 @@ export interface OnSubmitArgs{
 function GetLabeledElement(element: HTMLElement, text: string)
 {
     const label = document.createElement('LABEL') as HTMLLabelElement;
+    label.style.display = 'flex';
+    label.style.flexDirection = 'row';
     const textElement = document.createElement('TEXT');
     textElement.innerText = text;
     label.appendChild(element);
